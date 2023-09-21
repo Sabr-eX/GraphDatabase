fd266e3fbf2a2b45d75c3a142e4bac154761298b
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-09-21 07:04:43 +0530

    Initial Commit

---
+++ client.c
@@ -0,0 +1,194 @@
+/**
+ * @file client.c
+ * @author Divyateja Pasupuleti
+ * @brief
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * @copyright Copyright (c) 2023
+ * POSIX-compliant C program client.c
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+
+#define MESSAGE_LENGTH 100
+
+/**
+ * @brief The buffer structure for the message queue
+ * NOTE: Here operation = -1 would mean that we are getting response from server
+ */
+struct data
+{
+    int operation;
+    char message[MESSAGE_LENGTH];
+};
+
+struct msg_buffer
+{
+    long msg_type;
+    struct data data;
+};
+
+/**
+ * @brief The function to contact the Ping Server
+ * In this function, the client process will send a message to the Ping Server
+ * and wait for a response from the Ping Server. We will set operation to 1
+ * to indicate that we are contacting the Ping Server and set msg_type to the
+ * client id
+ */
+void server_ping(int msg_queue_id, int client_id, struct msg_buffer msg_buf)
+{
+    printf("[Client] Sending message to the Ping Server...\n");
+    msg_buf.data.message[0] = 'H';
+    msg_buf.data.message[1] = 'i';
+    msg_buf.data.message[2] = '\0';
+
+    msg_buf.msg_type = client_id;
+    msg_buf.data.operation = '1';
+
+    if (msgsnd(msg_queue_id, &msg_buf, sizeof(msg_buf.data), 0) == -1)
+    {
+        printf("[Client] Message could not be sent, please try again");
+    }
+    else
+    {
+        printf("Hello World.");
+
+        while (1)
+        {
+            if (msgrcv(msg_queue_id, &msg_buf, sizeof(msg_buf.data), msg_buf.msg_type, 0) == -1)
+            {
+                printf("[Client] Error while receiving message from the Ping Server");
+            }
+
+            printf("[Client] Message received from the Ping Server %ld: %s\n", msg_buf.msg_type, msg_buf.data.message);
+
+            if (msg_buf.msg_type == client_id && msg_buf.data.operation == 'r')
+            {
+                printf("[Client] Message received from the Ping Server: %s\n", msg_buf.data.message);
+                return;
+            }
+            else
+            {
+                // push the message back to the queue
+                if (msgsnd(msg_queue_id, &msg_buf, MESSAGE_LENGTH, 0) == -1)
+                {
+                    printf("[Client] Message could not be sent, please try again");
+                }
+            }
+        }
+    }
+}
+
+/**
+ * @brief The function to contact the File Search Server
+ *
+ */
+void server_file_search()
+{
+}
+
+/**
+ * @brief The function to contact the File Word Count Server
+ *
+ */
+void server_file_word_count()
+{
+}
+
+/**
+ * @brief The function to exit the client
+ *
+ */
+void server_exit()
+{
+}
+
+/**
+ * @brief On execution, each instance of this program creates a separate client process,
+ * i.e., if the executable file corresponding to client.c is client.out, then each time
+ * client.out is executed on a separate terminal, a separate client process is
+ * created.
+ *
+ * @return int 0
+ */
+int main()
+{
+    // Initialize the client
+    printf("Initializing Client...\n");
+
+    key_t key;
+    int msg_queue_id;
+    struct msg_buffer message;
+
+    // Generate key for the message queue
+    while ((key = ftok("key.md", 'B')) == -1)
+    {
+        printf("Error while generating key of the file");
+        exit(-1);
+    }
+
+    // Connect to the messsage queue
+    while ((msg_queue_id = msgget(key, 0644 | IPC_CREAT)) == -1)
+    {
+        printf("Error while connecting with Message Queue");
+        exit(-1);
+    }
+
+    printf("Successfully connected to the Message Queue %d %d\n", key, msg_queue_id);
+
+    // When a client process is run, it will ask the user to enter a positive integer as its client-id
+    int client_id;
+    printf("Enter Client-ID: ");
+    scanf("%d", &client_id);
+
+    // Display the menu
+    while (1)
+    {
+        printf("Choose from one of the options below: \n");
+        printf("1. Enter 1 to contact the Ping Server\n");
+        printf("2. Enter 2 to contact the File Search Server\n");
+        printf("3. Enter 3 to contact the File Word Count Server\n");
+        printf("4. Enter 4 if this Client wishes to exit\nInput: ");
+
+        int input;
+        scanf("%d", &input);
+
+        if (input == 1)
+        {
+            server_ping(msg_queue_id, client_id, message);
+        }
+        else if (input == 2)
+        {
+            server_file_search();
+        }
+        else if (input == 3)
+        {
+            server_file_word_count();
+        }
+        else if (input == 4)
+        {
+            return 0;
+        }
+        else
+        {
+            printf("Invalid Input. Please try again.\n");
+        }
+    }
+
+    // Destroy the message queue
+    if (msgctl(msg_queue_id, IPC_RMID, NULL) == -1)
+    {
+        printf("Error while destroying the message queue");
+        exit(-4);
+    }
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,183 @@
+/**
+ * @file server.c
+ * @author Divyateja Pasupuleti
+ * @brief
+ * @version 0.1
+ * @date 2023-09-20
+ *
+ * @copyright Copyright (c) 2023
+ * POSIX-compliant C program server.c
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+
+#define MESSAGE_LENGTH 100
+
+struct data
+{
+    int operation;
+    char message[MESSAGE_LENGTH];
+};
+
+struct msg_buffer
+{
+    long msg_type;
+    struct data data;
+};
+
+/**
+ * @brief Ping Server
+ *
+ */
+void ping(int msg_queue_id, int client_id, struct msg_buffer msg)
+{
+    int file_descriptor[2];
+    if (pipe(file_descriptor) == -1)
+    {
+        printf("Error: Could not create pipe");
+        exit(-1);
+    }
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("Error while creating child process");
+        exit(-1);
+    }
+    else if (pid == 0)
+    {
+        // Child Process
+        printf("[Child Process] Message received from Client %ld-Operation %c -> %s\n", msg.msg_type, msg.data.operation, msg.data.message);
+        printf("[Child Process] Sending message back to the client...\n");
+
+        // We send back hello
+        msg.data.message[0] = 'H';
+        msg.data.message[1] = 'e';
+        msg.data.message[2] = 'l';
+        msg.data.message[3] = 'l';
+        msg.data.message[4] = 'o';
+        msg.data.message[5] = '\0';
+
+        // We set message type as the client id
+        msg.msg_type = client_id;
+        msg.data.operation = 'r';
+
+        if (msgsnd(msg_queue_id, &msg, sizeof(msg.data), 0) == -1)
+        {
+            perror("[Child Process] Message could not be sent, please try again");
+            exit(-3);
+        }
+        else
+        {
+            printf("[Child Process] Message sent back to client %d successfully\n", client_id);
+        }
+    }
+    else
+    {
+        // Parent Process
+    }
+}
+
+/**
+ * @brief File Search Server
+ *
+ */
+void file_search()
+{
+}
+
+/**
+ * @brief File Word Count Server
+ *
+ */
+void file_word_count()
+{
+}
+
+/**
+ * @brief Cleanup
+ *
+ */
+void cleanup()
+{
+}
+
+/**
+ * @brief The main server is responsible for creating the message queue to be used for
+ * communication with the clients. The main server will listen to the message queue
+ * for new requests from the clients.
+ *
+ *
+ * @return int
+ */
+int main()
+{
+    // Iniitalize the server
+    printf("Initializing Server...\n");
+
+    // Create the message queue
+    key_t key;
+    int msg_queue_id;
+    struct msg_buffer msg;
+
+    // Link it with a key which lets you use the same key to communicate from both sides
+    if ((key = ftok("key.md", 'B')) == -1)
+    {
+        perror("Error while generating key of the file");
+        exit(-1);
+    }
+
+    // Create the message queue
+    if ((msg_queue_id = msgget(key, 0644 | IPC_CREAT)) == -1)
+    {
+        perror("Error while connecting with Message Queue");
+        exit(-1);
+    }
+
+    printf("Successfully connected to the Message Queue %d %d\n", key, msg_queue_id);
+
+    // Listen to the message queue for new requests from the clients
+    while (1)
+    {
+        if (msgrcv(msg_queue_id, &msg, sizeof(msg.data), 0, 0) == -1)
+        {
+            perror("Error while receiving message from the client");
+            exit(-2);
+        }
+
+        // printf("Message received from Client %ld-Operation %c -> %s\n", msg.msg_type, msg.data.operation, msg.data.message);
+
+        if (msg.data.operation == '1')
+        {
+            ping(msg_queue_id, msg.msg_type, msg);
+        }
+        else if (msg.data.operation == '2')
+        {
+            file_search();
+        }
+        else if (msg.data.operation == '3')
+        {
+            file_word_count();
+        }
+        else if (msg.data.operation == '4')
+        {
+            cleanup();
+        }
+    }
+
+    // Destroy the message queue
+    if (msgctl(msg_queue_id, IPC_RMID, NULL) == -1)
+    {
+        perror("Error while destroying the message queue");
+        exit(-4);
+    }
+
+    return 0;
+}
