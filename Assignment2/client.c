9981d40373674e51ad4c75ab140934112f4963cf
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-11-07 00:13:49 +0530

    feat: complete primary server for task 1

--- Assignment2/client.c
+++ Assignment2/client.c
@@ -41,8 +41,93 @@ struct msg_buffer
     struct data data;
 };
 
-void operation_one()
+void operation_one(int msg_queue_id, int client_id, int seq_num, struct msg_buffer message)
 {
+    // Input number of nodes
+    int number_of_nodes;
+    printf("Enter Number of Nodes: ");
+    scanf("%d", &number_of_nodes);
+
+    // Input adjacency matrix
+    int adjacency_matrix[number_of_nodes][number_of_nodes];
+    printf("Enter adjacency matrix, each row on a separate line and elements of a single row separated by whitespace characters: \n");
+    for (int i = 0; i < number_of_nodes; i++)
+    {
+        for (int j = 0; j < number_of_nodes; j++)
+        {
+            scanf("%d", &adjacency_matrix[i][j]);
+        }
+    }
+
+    // Connect to shared memory
+    key_t shm_key;
+    int shm_id;
+    // Generate key for the shared memory
+    // Here, we are using the client_id as the key because
+    // we want to ensure that each client has a unique shared memory
+    while ((shm_key = ftok(".", client_id)) == -1)
+    {
+        perror("[Client] Error while generating key for shared memory");
+        exit(EXIT_FAILURE);
+    }
+    printf("[Client] Generated shared memory key %d\n", shm_key);
+    // Connect to the shared memory using the key
+    if ((shm_id = shmget(shm_key, sizeof(adjacency_matrix) + sizeof(number_of_nodes), 0666 | IPC_CREAT)) == -1)
+    {
+        perror("[Client] Error occurred while connecting to shm\n");
+        return -1;
+    }
+    // Attach to the shared memory
+    int *shmptr = (int *)shmat(shm_id, NULL, 0);
+    if (shmptr == (void *)-1)
+    {
+        perror("[Client] Error while attaching to shared memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int shmptr_index = 0;
+    // Store data in shared memory using array traversals
+    shmptr[shmptr_index++] = number_of_nodes;
+    for (int i = 0; i < number_of_nodes; i++)
+    {
+        for (int j = 0; j < number_of_nodes; j++)
+        {
+            shmptr[shmptr_index++] = adjacency_matrix[i][j];
+        }
+    }
+
+    // Change message channel to load balancer and send it to load balancer
+    message.msg_type = LOAD_BALANCER_CHANNEL;
+    message.data.operation = 1;
+    message.data.seq_num = seq_num;
+
+    // Send the message to the load balancer
+    if (msgsnd(msg_queue_id, &message, sizeof(message.data), 0) == -1)
+    {
+        perror("[Client] Message could not be sent, please try again");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        while (msgrcv(msg_queue_id, &message, sizeof(message.data), client_id, 0) == -1)
+        {
+            perror("[Client] Error while receiving message from primary server");
+        }
+        printf("[Client] Message received from the Primary Server: %ld -> %s using %c\n", message.msg_type, message.data.graph_name, message.data.operation);
+        printf("[Client] File written successfully");
+    }
+
+    // Detach shared memory and delete it
+    if (shmdt(shmptr) == -1)
+    {
+        perror("[Client] Could not detach from shared memory\n");
+        exit(EXIT_FAILURE);
+    }
+    if (shmctl(shm_id, IPC_RMID, 0) == -1)
+    {
+        perror("[Client] Error while deleting the shared memory\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 /**
@@ -121,7 +206,7 @@ int main()
 
         if (operation == 1)
         {
-            operation_one();
+            operation_one(msg_queue_id, client_id, seq_num, message);
         }
         else if (operation == 2)
         {
--- Assignment2/primary_server.c
+++ Assignment2/primary_server.c
@@ -18,12 +18,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <limits.h>
+#include <sys/shm.h>
+#include <pthread.h>
 
 #define MESSAGE_LENGTH 100
 #define LOAD_BALANCER_CHANNEL 1
 #define PRIMARY_SERVER_CHANNEL 2
 #define SECONDARY_SERVER_CHANNEL_1 3
 #define SECONDARY_SERVER_CHANNEL_2 4
+#define MAX_THREADS 200
 
 struct data
 {
@@ -39,14 +42,108 @@ struct msg_buffer
     struct data data;
 };
 
+struct data_to_thread
+{
+    int msg_queue_id;
+    struct msg_buffer msg;
+};
+
 /**
  * @brief This function is executed by the thread which is responsible for writing to the new graph file
  *
  * @param arg
- * @return void**
+ * @return void*
  */
-void **writeToNewGraphFile(void **arg)
+void *writeToNewGraphFile(void **arg)
 {
+    struct data_to_thread *dtt = (struct data_to_thread *)arg;
+    // On the server side for storing data, we just start with an integer
+    // NOTE: Here we can use this and get away with it because we are not storing data here but only reading
+    // Refer: https://man7.org/linux/man-pages/man3/shmget.3p.html
+    int number_of_nodes;
+
+    // Connect to shared memory
+    key_t shm_key;
+    int shm_id;
+    // Generate key for the shared memory
+    // Here, we are using the client_id as the key because
+    // we want to ensure that each client has a unique shared memory
+    while ((shm_key = ftok(".", dtt->msg.data.client_id)) == -1)
+    {
+        perror("[Primary Server] Error while generating key for shared memory");
+        exit(EXIT_FAILURE);
+    }
+    printf("[Primary Server] Generated shared memory key %d\n", shm_key);
+    // Connect to the shared memory using the key
+    if ((shm_id = shmget(shm_key, sizeof(number_of_nodes), 0666)) == -1)
+    {
+        perror("[Primary Server] Error occurred while connecting to shm\n");
+        exit(EXIT_FAILURE);
+    }
+    // Attach to the shared memory
+    int *shmptr = (int *)shmat(shm_id, NULL, 0);
+    if (shmptr == (void *)-1)
+    {
+        perror("[Primary Server] Error in shmat \n");
+        exit(EXIT_FAILURE);
+    }
+
+    int shmptr_index = 0;
+    number_of_nodes = shmptr[shmptr_index++];
+    int adjacency_matrix[number_of_nodes][number_of_nodes];
+    for (int i = 0; i < number_of_nodes; i++)
+    {
+        for (int j = 0; j < number_of_nodes; j++)
+        {
+            adjacency_matrix[i][j] = shmptr[shmptr_index++];
+        }
+    }
+
+    // It's time to open the file and write the data to it
+    FILE *fp;
+    // Choose an appropriate size for your filename
+    char filename[256];
+    // Make sure the filename is null-terminated, and copy it to the 'filename' array
+    snprintf(filename, sizeof(filename), "graphs/%s", dtt->msg.data.graph_name);
+    fp = fopen(filename, "w");
+    if (fp == NULL)
+    {
+        perror("[Primary Server] Error while opening the file");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        // Write the data to the file
+        fprintf(fp, "%d\n", number_of_nodes);
+        for (int i = 0; i < number_of_nodes; i++)
+        {
+            for (int j = 0; j < number_of_nodes; j++)
+            {
+                fprintf(fp, "%d ", adjacency_matrix[i][j]);
+            }
+            fprintf(fp, "\n");
+        }
+        fclose(fp);
+    }
+    printf("[Primary Server] Successfully written to the file %s\n", filename);
+
+    // Send reply to the client
+    dtt->msg.msg_type = dtt->msg.data.client_id;
+    dtt->msg.data.operation = 0;
+    if (msgsnd(dtt->msg_queue_id, &(dtt->msg), sizeof(dtt->msg.data), 0) == -1)
+    {
+        perror("[Primary Server] Message could not be sent, please try again");
+        exit(EXIT_FAILURE);
+    }
+
+    // Detach from the shared memory
+    if (shmdt(shmptr) == -1)
+    {
+        perror("[Primary Server] Could not detach from shared memory\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("[Primary Server] Successfully Completed Operation 1\n");
+    pthread_exit(NULL);
 }
 
 /**
@@ -79,8 +176,10 @@ int main()
         perror("[Primary Server] Error while connecting with Message Queue");
         exit(EXIT_FAILURE);
     }
-
     printf("[Primary Server] Successfully connected to the Message Queue with Key:%d ID:%d\n", key, msg_queue_id);
+
+    // Store the thread_ids
+    pthread_t thread_ids[MAX_THREADS];
 
     // Listen to the message queue for new requests from the clients
     while (1)
@@ -92,7 +191,28 @@ int main()
         }
         else
         {
-            printf("[Primary Server] Received a message from Client: Op: %d File Name: %s\n", msg.data.operation, msg.data.graph_name);
+            printf("[Primary Server] Received a message from Client: Op: %ld File Name: %s\n", msg.data.operation, msg.data.graph_name);
+
+            if (msg.data.operation == 1)
+            {
+                // Write to a new file
+                struct data_to_thread dtt;
+                dtt.msg_queue_id = msg_queue_id;
+                dtt.msg = msg;
+                pthread_create(&thread_ids[msg.data.client_id], NULL, writeToNewGraphFile, (void *)&dtt);
+            }
+            else if (msg.data.operation == 2)
+            {
+                // Modify an existing file
+            }
+            else if (msg.data.operation == 5)
+            {
+                // Cleanup
+                for (int i = 0; i < 200; i++)
+                {
+                    pthread_join(thread_ids[i], NULL);
+                }
+            }
         }
     }
 
